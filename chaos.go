6e2a642990ad2cee6216c2168db36ca4d4e313d8
Raphaël Oester <raphaeloester@gmail.com>
2024-09-04 17:34:58 +0300

    adapt seed api for allowing variadic args

--- chaos.go
+++ chaos.go
@@ -9,24 +9,24 @@ import (
 )
 
 // Int returns a deterministic number between 0 and n included.
-func Int[I constraints.Integer](seed string, n I) I {
+func Int[I constraints.Integer](n I, seed ...any) I {
 	u := stringToSeed(seed)
 	r := rand.New(rand.NewSource(u))
 	return I(r.Int63n(int64(n)))
 }
 
 // Bool returns a deterministic boolean.
-func Bool(seed string) bool {
+func Bool(seed ...any) bool {
 	return stringToSeed(seed)%2 == 0
 }
 
 // Duration returns a deterministic duration between 0 and n.
-func Duration(seed string, n time.Duration) time.Duration {
-	return time.Duration(Int(seed, n.Nanoseconds()))
+func Duration(n time.Duration, seed ...any) time.Duration {
+	return time.Duration(Int(n.Nanoseconds(), seed))
 }
 
 // Float returns a deterministic float between 0 and n
-func Float[F constraints.Float](seed string, n F) F {
+func Float[F constraints.Float](n F, seed ...any) F {
 	u := stringToSeed(seed)
 	r := rand.New(rand.NewSource(u))
 	return F(r.Float64()) * n
@@ -35,28 +35,27 @@ func Float[F constraints.Float](seed string, n F) F {
 const alphanumericalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 // String returns a deterministic string of length n.
-func String(seed string, length int) string {
+func String(length int, seed ...any) string {
 	ret := ""
 	for i := 0; i < length; i++ {
-		iSeed := fmt.Sprintf("%s-%d", seed, i)
-		index := Int(iSeed, len(alphanumericalChars)-1)
+		index := Int(len(alphanumericalChars)-1, append(seed, i))
 		ret += string(alphanumericalChars[index])
 	}
 	return ret
 }
 
 // SliceItem returns a deterministic item from a slice.
-func SliceItem[T any](seed string, items []T) T {
+func SliceItem[T any](items []T, seed ...any) T {
 	var t T
 	if len(items) == 0 {
 		return t
 	}
-	return items[Int(seed, len(items)-1)]
+	return items[Int(len(items)-1, seed)]
 }
 
 // UniqueSliceItems returns a slice with a length of count.
 // The items are unique.
-func UniqueSliceItems[T any](seed string, items []T, count int) ([]T, error) {
+func UniqueSliceItems[T any](items []T, count int, seed ...any) ([]T, error) {
 	if len(items) < count {
 		return nil, fmt.Errorf("not enough items to select from: %d < %d", len(items), count)
 	}
@@ -64,7 +63,7 @@ func UniqueSliceItems[T any](seed string, items []T, count int) ([]T, error) {
 	selectedItems := make([]T, 0, count)
 	availableItems := append([]T(nil), items...)
 	for i := 0; i < count; i++ {
-		index := Int(fmt.Sprintf("%s-%d", seed, i), len(availableItems)-1)
+		index := Int(len(availableItems)-1, seed, i)
 		selectedItems = append(selectedItems, availableItems[index])
 		availableItems = append(availableItems[:index], availableItems[index+1:]...)
 	}
@@ -76,7 +75,7 @@ func UniqueSliceItems[T any](seed string, items []T, count int) ([]T, error) {
 // The items are unique.
 // If there are not enough items to select from, it panics.
 func MustUniqueSliceItems[T any](seed string, items []T, count int) []T {
-	selectedItems, err := UniqueSliceItems(seed, items, count)
+	selectedItems, err := UniqueSliceItems(items, count, seed)
 	if err != nil {
 		panic(err)
 	}
@@ -88,7 +87,7 @@ func MustUniqueSliceItems[T any](seed string, items []T, count int) []T {
 func IntSlice(seed string, high int, length int) []int {
 	result := make([]int, length)
 	for i := 0; i < length; i++ {
-		result[i] = Int(fmt.Sprintf("%s-%d", seed, i), high)
+		result[i] = Int(high, seed, i)
 	}
 
 	return result
--- helper.go
+++ helper.go
@@ -3,9 +3,10 @@ package chaos
 import (
 	"crypto/sha256"
 	"encoding/binary"
+	"fmt"
 )
 
-func stringToSeed(s string) int64 {
-	hash := sha256.Sum256([]byte(s))
+func stringToSeed(s []any) int64 {
+	hash := sha256.Sum256([]byte(fmt.Sprint(s...)))
 	return int64(binary.BigEndian.Uint64(hash[:8]))
 }
