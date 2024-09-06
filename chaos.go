907024e2cf387e914fd86858f07b16caf06e81d2
Ivan <apedoivan@gmail.com>
2024-09-06 16:10:48 +0200

    pull & merge

--- chaos.go
+++ chaos.go
@@ -5,45 +5,20 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math/rand"
-	"time"
-
-	"github.com/google/uuid"
-	"golang.org/x/exp/constraints"
 )
 
-// Int returns a deterministic number between 0 and n included.
-func Int[I constraints.Integer](n I, seed ...any) I {
-	u := stringToSeed(seed)
-	r := rand.New(rand.NewSource(u))
-	return I(r.Int63n(int64(n)))
+type Chaos struct {
+	count int
+	fixed bool
+	seed  string
 }
 
-// MustIntInRange returns a deterministic integer between min and max (inclusive).
-// It panics if max is less than min.
-func MustIntInRange(min, max int, seed ...any) int {
-	if max < min {
-		panic("max must be greater than or equal to min")
+func New(seed string) *Chaos {
+	return &Chaos{
+		count: 0,
+		fixed: false,
+		seed:  seed,
 	}
-	return min + Int(max-min, seed...)
-}
-
-// Bool returns a deterministic boolean.
-func Bool(seed ...any) bool {
-	return stringToSeed(seed)%2 == 0
-}
-
-// Duration returns a deterministic duration between 0 and n.
-func Duration(n time.Duration, seed ...any) time.Duration {
-	return time.Duration(Int(n.Nanoseconds(), seed))
-}
-
-// MustDurationInRange returns a deterministic duration between min and max.
-// It panics if max is less than min.
-func MustDurationInRange(min, max time.Duration, seed ...any) time.Duration {
-	if max < min {
-		panic("max must be greater than or equal to min")
-	}
-	return min + Duration(max-min, seed...)
 }
 
 // Fix freezes the chaos.
